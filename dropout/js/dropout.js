1dae796e26389caad03a1cc938da4a1e802b3fbe
eddygulled <edgargulay69@gmail.com>
2024-03-14 21:11:40 +0300

    Refactor form field labels and add AJAX post request

--- dropout/js/dropout.js
+++ dropout/js/dropout.js
@@ -1,6 +1,10 @@
+let xhttp = new XMLHttpRequest();
+const URL = "https://oyster-app-c8hnb.ondigitalocean.app/";
+
+
 $(document).ready(() => {
 
-    $(".inputs").on("change", () => {
+    function getFormValues() {
         let formValues = {};
 
         document.querySelectorAll('form').forEach((form) => {
@@ -16,9 +20,47 @@ $(document).ready(() => {
         });
 
         console.log(formValues);
+        $("#age_value").html(formValues.age + " years");
+        return formValues;
+    }
+
+    $(".inputs").on("change", ()=>{
+        sendPostRequest(getFormValues()).then((response) => {
+            console.log(response);
+            if (response.status === 200) {
+                console.log(response.data.result);
+            }
+        }, (error) => {
+            console.log(error);
+            console.log("Error: " + error.status + " " + error.data);
+        });
     });
-
-
-    
-
 });
+
+
+
+function sendPostRequest(formValues) {
+    return new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    // The request has been processed successfully
+                    resolve({status: this.status, data: JSON.parse(this.responseText)});
+                } else {
+                    // There was an error with the request
+                    reject({status: this.status, data: this.responseText});
+                }
+            }
+        };
+
+        xhttp.open("POST", URL, true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+
+        // Convert the formValues object to a JSON string
+        let data = JSON.stringify(formValues);
+
+        xhttp.send(data);
+    });
+}
