16174773fa6e6a8f694259ac1292cd2d5ccdd611
eddygulled <edgargulay69@gmail.com>
2024-03-14 20:54:25 +0300

    Refactor form input handling and update select styles

--- dropout/js/dropout.js
+++ dropout/js/dropout.js
@@ -1,16 +1,24 @@
 $(document).ready(() => {
 
     $(".inputs").on("change", () => {
-        //  log the value of the input
-        console.log($(".inputs").val());
+        let formValues = {};
+
+        document.querySelectorAll('form').forEach((form) => {
+            form.querySelectorAll('input, select').forEach((input) => {
+                if (input.type === "radio") {
+                    if (input.checked) {
+                        formValues[input.name] = input.value;
+                    }
+                } else {
+                    formValues[input.name] = input.value;
+                }
+            });
+        });
+
+        console.log(formValues);
     });
 
 
-
-
-
-
-
-
+    
 
 });
